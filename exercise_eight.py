6722e18750a62fe78a830865d7e6ea283a61a06f
Angela Ferrer <gelferrero01@gmail.com>
2024-01-25 23:56:58 +0800

    displaying pattern

---
+++ exercise_eight.py
@@ -0,0 +1,7 @@
+# Create a half triangle number pattern comprised of numbers 1 to 5 in ascending order. 
+
+for numbers in range (5):
+    for i in range (numbers):
+    # Display the pattern
+        print (numbers, end="")
+        print ('')
